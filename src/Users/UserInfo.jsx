8b952cd24265aac45644c0d58c9a2b4be38eaf2a
VadymChorny <chorrny228@gmail.com>
2022-07-31 17:07:19 +0300

    add example

--- src/Users/UserInfo.jsx
+++ src/Users/UserInfo.jsx
@@ -18,7 +18,12 @@ export default function UserInfo(props) {
   return (
     <div>
       <h1>Login: {user?.login}</h1>
-      <img src={user?.avatar_url} />
+      <img
+        src={
+          user?.avatar_url ??
+          'https://cdn-icons.flaticon.com/png/512/5752/premium/5752574.png?token=exp=1659277049~hmac=9a256d065b9ab5293914b0910537afa2'
+        }
+      />
     </div>
   );
 }
